fc8c6bbe514932a30241db1c4ec534779f1edb99
2Pankaj <115923198+2Pankaj@users.noreply.github.com>
2023-06-05 07:49:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import { fileURLToPath } from 'url';
 dotenv.config();
 
 //databse config
-const  __filename = fileUrlToPath(import.meta.url);
+const  __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 connectDB();
 
